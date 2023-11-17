d2c6829d1819846fa0fa3c999f7d3b6050a1eec8
Miguel Espinel <miguele2141@gmail.com>
2023-11-16 20:13:12 -0500

    first commit with files

---
+++ src/main/java/com/entornos/EntornosP2Backend/EntornosP2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.entornos.EntornosP2Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntornosP2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntornosP2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entornos/EntornosP2Backend/EntornosP2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entornos.EntornosP2Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntornosP2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
